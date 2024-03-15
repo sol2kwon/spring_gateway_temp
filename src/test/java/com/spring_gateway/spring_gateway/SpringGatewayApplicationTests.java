a52217eb46d6b8342b5555ee6aea453ff3c781e8
sol2kwon <amorfatisole@gmail.com>
2024-03-15 15:27:08 +0900

    first commit

---
+++ src/test/java/com/spring_gateway/spring_gateway/SpringGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_gateway.spring_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
