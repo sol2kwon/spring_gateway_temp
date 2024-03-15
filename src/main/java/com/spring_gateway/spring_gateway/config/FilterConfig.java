e4230284f6336a896916de86306219cf67a22b1a
sol2kwon <amorfatisole@gmail.com>
2024-03-15 17:57:44 +0900

    filter2

--- src/main/java/com/spring_gateway/spring_gateway/config/FilterConfig.java
+++ src/main/java/com/spring_gateway/spring_gateway/config/FilterConfig.java
@@ -1,13 +1,13 @@
-package com.spring_gateway.spring_gateway;
+package com.spring_gateway.spring_gateway.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class FilterConfig {
-    @Bean
+   // @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/first-service/**")
